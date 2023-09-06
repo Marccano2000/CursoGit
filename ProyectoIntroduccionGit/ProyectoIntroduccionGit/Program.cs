ad9e3a6c3392ad0d9bf244562cacfd90d4989fea
marcCano <marc.cano@students.salle.url.edu>
2023-09-06 13:32:27 +0200

    Parrafo de fuente Proyecto GIT

---
+++ ProyectoIntroduccionGit/ProyectoIntroduccionGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProyectoIntroduccionGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Este proyeto es una introduccion a git!!");
+        }
+    }
+}
