ec3c8ee76dd005e7d43b0f7d4b70f4b2d0f7f5ca
marcCano <marc.cano@students.salle.url.edu>
2023-09-06 18:02:05 +0200

    Creacion de conflicto entre master y claseProgramClasse1

--- ProyectoIntroduccionGit/ProyectoIntroduccionGit/Program.cs
+++ ProyectoIntroduccionGit/ProyectoIntroduccionGit/Program.cs
@@ -8,6 +8,8 @@ namespace ProyectoIntroduccionGit
         {
 
             Console.WriteLine("Este proyeto es una introduccion a git!!");
+
+            Console.WriteLine("Esta modificacion es para poder crear un conflicto de prueba i hacer un merch!");
         }
     }
 }
